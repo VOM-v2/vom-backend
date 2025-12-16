731f064e1fe9a41f8f5168e826e2462f50b35caa
okodee <okodee@naver.com>
2025-12-16 21:42:05 +0900

    Initialize Spring Boot project (#5)

---
+++ src/main/java/okodee/vom/VomApplication.java
@@ -0,0 +1,13 @@
+package okodee.vom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/okodee/vom/VomApplicationTests.java
@@ -0,0 +1,13 @@
+package okodee.vom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
