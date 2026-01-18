7e9401bab9621095f436c1963749546457ce7b2e
okodeee <120557382+okodeee@users.noreply.github.com>
2026-01-18 16:37:53 +0900

    Revert "[Feature] 인증 기능 구현"

--- src/main/java/okodee/vom/domain/user/entity/Gender.java
+++
@@ -1,7 +0,0 @@
-package okodee.vom.domain.user.entity;
-
-public enum Gender {
-    MALE,
-    FEMALE,
-    OTHER
-}
--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
--- 사용자 더미 데이터
-INSERT INTO users (id, email, name, password, created_at, is_deleted, locked, role) VALUES
-('550e8400-e29b-41d4-a716-446655440001', 'test@email.com', 'test', '$2a$12$.AmgP4Hqjw3a3ah5/Wo.AuxJoF1mMlP5y5N4ZNucrz4V.fslF1Koi', NOW(), false, false, 'USER'),
-('550e8400-e29b-41d4-a716-446655440002', 'test1@email.com', 'test1', '$2a$12$.AmgP4Hqjw3a3ah5/Wo.AuxJoF1mMlP5y5N4ZNucrz4V.fslF1Koi', NOW(), false, false, 'USER');
