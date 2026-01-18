7f995e974ef030645f3520dbead93c109eb536ba
okodeee <120557382+okodeee@users.noreply.github.com>
2026-01-18 16:48:20 +0900

    Revert "[Feature] 인증 기능 구현"

--- src/main/java/okodee/vom/global/exception/ErrorCode.java
+++ src/main/java/okodee/vom/global/exception/ErrorCode.java
@@ -4,11 +4,6 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-    // Auth
-    DUPLICATE_EMAIL("이미 가입된 이메일입니다."),
-    INVALID_TOKEN("토큰이 유효하지 않습니다."),
-    INVALID_USER_DETAILS("사용자 인증 정보(UserDetails)가 유효하지 않습니다."),
-
     // Server 에러 코드
     INTERNAL_SERVER_ERROR("서버 내부 오류가 발생했습니다."),
     INVALID_REQUEST("잘못된 요청입니다.");
