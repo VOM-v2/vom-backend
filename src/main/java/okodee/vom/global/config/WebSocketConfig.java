0530552633ba7ade9d35b15392bca78fbe5c1cb4
okodee <okodee@naver.com>
2026-03-18 15:07:54 +0900

    feat: WebSocket Config 설정 (#21)

---
+++ src/main/java/okodee/vom/global/config/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package okodee.vom.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 구독 경로 prefix
+        registry.enableSimpleBroker("/topic", "/queue");
+        // 송신 경로 prefix
+        registry.setApplicationDestinationPrefixes("/app");
+        // 특정 사용자에게 알림 보낼 때 prefix (/user/queue/notifications)
+        registry.setUserDestinationPrefix("/user");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+            .setAllowedOriginPatterns("*")
+            .withSockJS();
+    }
+}
